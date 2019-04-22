2005c8b41c81f63e07309bf38cf33f7bcb746c87
Trevor <tstike44@hotmail.com>
2019-04-21 21:44:52 -0700

    got all of the files initial contents/connections set up

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+const connection = require("./connection.js");
+
+let orm = {
+
+    selectAll: function(){
+
+    },
+    insertOne: function(){
+
+    },
+    updateOne: function() {
+
+    }
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers(id, burger_name, devoured)
+VALUES (1, "Bologna Burger", false), (2, "Pastrami Burger", false), (3, "Bolognase Burger", false);
